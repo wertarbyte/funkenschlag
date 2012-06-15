3096f74fda19b1154a00fc35373d2f8c50a5ceff
Stefan Tomanek <stefan@pico.ruhr.de>
2012-06-15 16:28:45 +0200

    add gimbal adjustment to Datenschlag

--- funkenschlag.c
+++ funkenschlag.c
@@ -274,6 +274,9 @@ int main(void) {
 		/* check switch for Datenschlag */
 		static uint8_t old_switch = 0;
 		uint8_t ds_payload[DS_MAX_PAYLOAD_LENGTH] = {0};
+		#define DS_CMD_ANY 0xFF
+#define SEND_AUX_SWITCHES
+#ifdef SEND_AUX_SWITCHES
 		for (uint8_t i=SW_CHANNELS; i<N_3W_SWITCHES; i++) {
 			switch (sw_positions[i]) {
 				case 0:
@@ -288,7 +291,6 @@ int main(void) {
 					break;
 			}
 		}
-		#define DS_CMD_ANY 0xFF
 		#define DS_CMD_AUX (1<<5| 0x0A)
 		/* if the switch state changes, remove obsolete frames from the queue */
 		if (old_switch != ds_payload[0]) {
@@ -300,6 +302,7 @@ int main(void) {
 			/* 0x2A: 001 01010 */
 			ds_add_frame(DS_CMD_AUX, &ds_payload[0], 1);
 		}
+#endif
 //#define SEND_MAG_HEADING
 #ifdef SEND_MAG_HEADING
 		/* send orientation */
@@ -309,6 +312,24 @@ int main(void) {
 		ds_payload[1] = dir&0x0F;
 		if (!ds_frame_queued(DS_CMD_SET_HEADING) && ds_frame_buffers_available()) {
 			ds_add_frame(DS_CMD_SET_HEADING, &ds_payload[0], 2);
+		}
+#endif
+//#define SEND_GIMBAL_ANGLE
+#ifdef SEND_GIMBAL_ANGLE
+		#define DS_GIMBAL_ANGLE_THRESHOLD 30
+		/* send orientation */
+		#define DS_CMD_SET_GIMBAL (1<<5 | 0x0C)
+		/* angle in a range from 0 to 255 (180°) */
+		static uint8_t last_angle = 0;
+		uint8_t angle = 0; // set desired angle here
+		ds_payload[0] = angle;
+		/* if an angle change exceeds the threshold, abort other messages and transmit at once */
+		if ((angle > last_angle && angle-last_angle >= DS_GIMBAL_ANGLE_THRESHOLD) || (angle < last_angle && last_angle-angle >= DS_GIMBAL_ANGLE_THRESHOLD) ) {
+			while (ds_abort_frame(DS_CMD_ANY));
+			last_angle = angle;
+		}
+		if (!ds_frame_queued(DS_CMD_SET_GIMBAL) && ds_frame_buffers_available()) {
+			ds_add_frame(DS_CMD_SET_GIMBAL, &ds_payload[0], 1);
 		}
 #endif
 	}
